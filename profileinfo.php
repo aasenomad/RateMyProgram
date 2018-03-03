4600e539ac182f8f17d8f4a8218a7426d2c5178d
Nolyna <nsoumbou1@student.gsu.edu>
2018-03-03 11:05:37 -0500

    profile

--- profileinfo.php
+++ profileinfo.php
@@ -1,42 +1,48 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/profile.css">
-    <title>Profile!</title>
-  </head>
-  <body>
-    <div class="container">
 
-      <div class="row">
 
-           <div class="col-md-7 ">
-             <div class="card">
-               <div class="card-body">
-                 <h5 class="card-title">Special title treatment</h5>
-                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Cras justo odio</li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
-                    <li class="list-group-item">Morbi leo risus</li>
-                    <li class="list-group-item">Porta ac consectetur ac</li>
-                    <li class="list-group-item">Vestibulum at eros</li>
-                  </ul>
-                 <a href="#" class="btn btn-primary">Go somewhere</a>
-               </div>
-            </div>
-        </div> <!-- end col-->
-      </div> <!-- end row-->
+
+<div class="col-md-6 ">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Basic Information </h5>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"> Username : <span class="modal-text d-flex justify-content-sm-end"  data-toggle="modal" data-target="#usernameModal"> Edit </span> </li>
+        <li class="list-group-item">Email : | <span class="modal-text"  data-toggle="modal" data-target="#emailModal"> Edit </span></li>
+          <li class="list-group-item">Password : ******** | <span class="modal-text"  data-toggle="modal" data-target="#pswModal"> Edit </span></li>
+        <li class="list-group-item"> School: | <span class="modal-text"  data-toggle="modal" data-target="#schoolModal"> Edit </span></li>
+        <li class="list-group-item">Position: | <span class="modal-text"  data-toggle="modal" data-target="#positionModal"> Edit </span></li>
+        <!--li class="list-group-item">Vestibulum at eros</li-->
+      </ul>
     </div>
+  </div>
+</div> <!-- end col-->
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
+<!------------------------------->
+
+
+<!-- Modal -->
+<div class="modal fade" id="usernameModal" tabindex="-1" role="dialog" aria-labelledby="usernameModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="usernameModalLabel">Update username</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form class="form-inline">
+          <div class="form-group">
+            <label for="staticUsername" class="sr-only">New:</label>
+            <input type="text" class="form-control" id="staticUsername" name="username">
+          </div>
+        <form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
