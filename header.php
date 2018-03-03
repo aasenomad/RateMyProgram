0bd51710c81a6438361e4d0648f365a5f9ec98a9
Jaime Torres <jtorres16@mail.usf.edu>
2018-03-03 11:01:35 -0500

    index-heder

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+
+<form class="form-inline">
+<label class="sr-only" for="inlineFormEmail">Username</label>
+<input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputEmail" placeholder="Email">
+
+<label class="sr-only" for="inlineFormInputGroupPassword">Password</label>
+<input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputGroupPassword" placeholder="Password">
+
+<button type="submit" class="btn btn-primary mb-2">Submit</button>
+<a href="signup.php" type="button" class="btn btn-primary mb-2">Sign Up</a>
+</form>
+
+
+</header>
---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rate My Program - Review Graduate Programs Nationwide</title>
+	<style>
+		nav {
+			background-color: black;
+			height: 50px;
+		}
+
+		nav ul {
+
+		}
+
+		nav ul li {
+			list-style-type: none;
+			width: 300px;
+			float: left;
+			border-right: 1px solid #CCC;
+			text-align: center;
+		}
+
+		li a {
+			text-decoration: none;
+			color: white;
+			line-height: 50px;
+			display: block;
+		}
+
+		li a:hover {
+			background-color: yellow;
+			color: green;
+		}
+
+		form{
+			float: right;
+		}
+
+		label {
+
+		}
+
+		input {
+
+}
+
+		div {
+			float: middle;
+		}
+
+		div input {
+
+		}
+	</style>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+	<br>
+
+	<?php include 'header.php'; ?>
+<br>
+<br>
+<nav>
+	<ul>
+		<li><a href="#">Profile</a></li>
+		<li><a href="#">About Us</a></li>
+</nav>
+<br>
+<br>
+<h1><center>Find my Graduate Program</center></h1>
+<button type="button" class="btn btn-primary btn-lg btn-block">Graduate Program</button>
+<br>
+<h1><center>Find my College/University</center></h1>
+<button type="button" class="btn btn-primary btn-lg btn-block">College/University</button>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
