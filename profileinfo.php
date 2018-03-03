4387353a4cae629a03c00728fa8d63da31989cb5
Nolyna <nsoumbou1@student.gsu.edu>
2018-03-03 16:27:29 -0500

    profile tabs
    css + tab pages

--- profileinfo.php
+++ profileinfo.php
@@ -8,10 +8,10 @@
       <h5 class="card-title">Basic Information </h5>
       <ul class="list-group list-group-flush">
         <li class="list-group-item"> Username : <span class="modal-text d-flex justify-content-sm-end"  data-toggle="modal" data-target="#usernameModal"> Edit </span> </li>
-        <li class="list-group-item">Email : | <span class="modal-text"  data-toggle="modal" data-target="#emailModal"> Edit </span></li>
-          <li class="list-group-item">Password : ******** | <span class="modal-text"  data-toggle="modal" data-target="#pswModal"> Edit </span></li>
-        <li class="list-group-item"> School: | <span class="modal-text"  data-toggle="modal" data-target="#schoolModal"> Edit </span></li>
-        <li class="list-group-item">Position: | <span class="modal-text"  data-toggle="modal" data-target="#positionModal"> Edit </span></li>
+        <li class="list-group-item">Email :  <span class="modal-text d-flex justify-content-sm-end"  data-toggle="modal" data-target="#emailModal"> Edit </span></li>
+          <li class="list-group-item">Password : ******** <span class="modal-text d-flex justify-content-sm-end"  data-toggle="modal" data-target="#pswModal"> Edit </span></li>
+        <li class="list-group-item"> School:  <span class="modal-text d-flex justify-content-sm-end"  data-toggle="modal" data-target="#schoolModal"> Edit </span></li>
+        <li class="list-group-item">Position: <span class="modal-text d-flex justify-content-sm-end"  data-toggle="modal" data-target="#positionModal"> Edit </span></li>
         <!--li class="list-group-item">Vestibulum at eros</li-->
       </ul>
     </div>
@@ -21,7 +21,7 @@
 <!------------------------------->
 
 
-<!-- Modal -->
+<!-- Modal username -->
 <div class="modal fade" id="usernameModal" tabindex="-1" role="dialog" aria-labelledby="usernameModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -31,17 +31,126 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
-        <form class="form-inline">
+      <form class="form-inline">
+        <div class="modal-body">
           <div class="form-group">
             <label for="staticUsername" class="sr-only">New:</label>
             <input type="text" class="form-control" id="staticUsername" name="username">
           </div>
-        <form>
-      </div>
+        </div>
+      </form>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal email -->
+<div class="modal fade" id="emailModal" tabindex="-1" role="dialog" aria-labelledby="emailModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="emailModalLabel">Update email</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form class="form-inline">
+        <div class="modal-body">
+            <div class="form-group">
+              <label for="staticEmail" class="sr-only">New:</label>
+              <input type="email" class="form-control" id="staticEmail" name="email" required>
+            </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save changes</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- Modal password -->
+<div class="modal fade" id="pswModal" tabindex="-1" role="dialog" aria-labelledby="pswModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="usernameModalLabel">Update Password</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form class="form-inline">
+        <div class="modal-body">
+          <div class="form-group">
+            <label for="staticpwd" class="sr-only">Current:</label>
+            <input type="password" class="form-control" id="staticpwd" name="pwd1">
+          </div>
+          <div class="form-group">
+            <label for="staticpwd" class="sr-only">New:</label>
+            <input type="password" class="form-control" id="staticpwd" name="pwd2">
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save changes</button>
+        </div>
+      <form>
+    </div>
+  </div>
+</div>
+
+<!-- Modal School -->
+<div class="modal fade" id="schoolModal" tabindex="-1" role="dialog" aria-labelledby="SchoolModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="SchoolModalLabel">Update School</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form class="form-inline">
+        <div class="modal-body">
+          <div class="form-group">
+            <label for="staticSchool" class="sr-only">New:</label>
+            <input type="text" class="form-control" id="staticSchool" name="School
+            ">
+          </div>
+        </div>
+      </form>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal position -->
+<div class="modal fade" id="positionModal" tabindex="-1" role="dialog" aria-labelledby="positionModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="positionModalLabel">Update Position</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form class="form-inline">
+        <div class="modal-body">
+          <div class="form-group">
+            <label for="staticPosition" class="sr-only">New:</label>
+            <input type="text" class="form-control" id="staticPosition" name="position">
+          </div>
+        </div>
+      </form>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
---
+++ saveprogramtab.php
@@ -0,0 +1,26 @@
+<ul class="list-group">
+  <li class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1"> Florida State University </h5>
+      <small>3 days ago</small>
+    </div>
+    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <small> Overall Rating: *****  3.5 | review: 5000 | <a> more details </a> </small>
+  </li>
+  <li href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <small class="text-muted">Donec id elit non mi porta.</small>
+  </li>
+  <li class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <small class="text-muted">Donec id elit non mi porta.</small>
+  </li>
+</ul>
