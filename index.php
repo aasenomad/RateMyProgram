8b8053815f0c850b4ad0d036c016802d80bb1ad1
Jaime Torres <jtorres16@mail.usf.edu>
2018-03-03 23:26:22 -0500

    remove empty style

--- index.php
+++ index.php
@@ -38,19 +38,6 @@
 			background-color: yellow;
 			color: green;
 		}
-
-
-		label {
-
-		}
-
-		input {
-
-		}
-
-		div {
-
-		}
 	</style>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
