e87760aedb000a373b5d8ded66ae9206df728322
jsianguk <37007141+jsianguk@users.noreply.github.com>
2018-03-04 05:06:28 -0500

    Update RatingPage.php

--- RatingPage.php
+++ RatingPage.php
@@ -1,6 +1,27 @@
+@ -1,41 +1,18 @@
+ <?php
+
 <?php
 session_start();
 include 'includes/dbconnect.php';
+include 'includes/function/userdetails.php';
+
+if ( isset($_GET['program'])){
+    $schoolId = $_GET['program'];
+
+}else{$schoolId = 1; } 
+        
+
+if( isset($_POST['submission']) ){
+    $review($_POST['review'],$schoolId);
+    $rate(1,$_POST['option1'],$schoolId);
+    $rate(2,$_POST['option2'],$schoolId);
+    $rate(3,$_POST['option3'],$schoolId);
+    $rate(4,$_POST['option4'],$schoolId);
+    $rate(5,$_POST['option5'],$schoolId);
+    $rate(6,$_POST['option6'],$schoolId);
+    
+
 include 'includes/function/ratings.php';
 if(isset($_GET['sid'])){
     $idschool = $_GET['sid'];
@@ -10,16 +31,23 @@ if(isset($_GET['sid'])){
 
 ?>
 
+
+
 <!DOCTYPE html>
 <html lang="en">
+  
+    <head>
+        
+            <?php 
+    include 'header.php';
+?>
+    
+  
   <head>
       <!-- Bootstrap meta tags -->
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <!---->
-<!---bootstrap-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <!----->
+@ -46,185 +23,56 @@ if( isset($_POST['submission']) ){
     <title>Ratings Page</title>
   </head>
 <body>
@@ -29,6 +57,12 @@ if(isset($_GET['sid'])){
 
 <div class="container">
 <!--program identier-->
+<ul class="list-group list-group-flush">
+  <li class="list-group-item">Program Name</li>
+  <li class="list-group-item">Degree</li>
+  <li class="list-group-item">Location</li>
+  <li class="list-group-item">Program Type</li>
+</ul>
 <?php
     $prog = getSchool($idschool);
     echo '
@@ -42,7 +76,11 @@ if(isset($_GET['sid'])){
     ';
 ?>
 
+<br><br>
 
+        
+        
+        
 <br><br>
 <!---->
 
@@ -65,18 +103,178 @@ if(isset($_GET['sid'])){
         <strong>Do</strong>
         Double check your comments before posting. It doesn’t hurt to check grammar
     </p>
+      
     <!---bootstrap form-->
+ <form method="post" action="#">
+    <div class="form-group">
+      <label for="ProsConsTextarea">Pros and Cons</label>
+      <textarea class="form-control" name="review" id="ProsConsTextarea" rows="5"
+        ></textarea>
+    </div>
+        
 
+            <!--Rating Description-->
 
+    <p>Comments</p>
+    <div id="mem" style="margin-left:10px">  
+          <label> Overall rating: </label>
+    </div>   
+
+    <!--Star Rating -------------- -->
+    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+     
+      <label class="btn btn-secondary active">
+        <input type="radio" name="option1" id="option1" value="1" autocomplete="off" checked> 1
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option1" id="option2" value="2" autocomplete="off"> 2
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option1" id="option3" value="3" autocomplete="off"> 3
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option1" id="option4" value="4" autocomplete="off"> 4
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option1" id="option5" value="4" autocomplete="off"> 5
+      </label>
+    </div>
+        
+          <div class="btn-group btn-group-toggle" data-toggle="buttons">
+              
+              <div id="mem" style="margin-left:10px">  
+          <label> level of difficulty: </label>
+    </div>   
+
+      <label class="btn btn-secondary active">
+        <input type="radio" name="option2" id="option1" value="1" autocomplete="off" checked> 1
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option2" id="option2" value="2" autocomplete="off"> 2
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option2" id="option3" value="3" autocomplete="off"> 3
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option2" id="option4" value="4" autocomplete="off"> 4
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option2" id="option5" value="5" autocomplete="off"> 5
+      </label>
+    </div>
+        
+           <div class="btn-group btn-group-toggle" data-toggle="buttons">
+               <div id="mem" style="margin-left:10px">  
+          <label> Academic quality: </label>
+    </div>   
+
+      <label class="btn btn-secondary active">
+        <input type="radio" name="option3" id="option1" value="1" autocomplete="off" checked> 1
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option3" id="option2" value="2" autocomplete="off"> 2
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option3" id="option3" value="3" autocomplete="off"> 3
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option3" id="option4" value="4" autocomplete="off"> 4
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option3" id="option5" value="5" autocomplete="off"> 5
+      </label>
+    </div>
+        
+        
+                
+           <div class="btn-group btn-group-toggle" data-toggle="buttons">
+               <div id="mem" style="margin-left:10px">  
+          <label> Research Opportunity: </label>
+    </div>   
+
+      <label class="btn btn-secondary active">
+        <input type="radio" name="option4" id="option1" value="1" autocomplete="off" checked> 1
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option4" id="option2" value="2" autocomplete="off"> 2
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option4" id="option3" value="3" autocomplete="off"> 3
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option4" id="option4" value="4" autocomplete="off"> 4
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option4" id="option5" value="5" autocomplete="off"> 5
+      </label>
+    </div>
+        
+        
+                     
+           <div class="btn-group btn-group-toggle" data-toggle="buttons">
+               <div id="mem" style="margin-left:10px">  
+          <label> Location: </label>
+    </div>   
+   
+      <label class="btn btn-secondary active">
+        <input type="radio" name="option5" id="option1" value="1" autocomplete="off" checked> 1
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option5" id="option2" value="2" autocomplete="off"> 2
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option5" id="option3" value="3" autocomplete="off"> 3
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option5" id="option4" value="4" autocomplete="off"> 4
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option5" id="option5" value="5" autocomplete="off"> 5
+      </label>
+    </div>
+        
+        
+                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+               <div id="mem" style="margin-left:10px">  
+          <label> Atmosphere (Respectful, Friendly, Fun people): </label>
+    </div>   
+   
+      <label class="btn btn-secondary active">
+        <input type="radio" name="option6" id="option1" value="1" autocomplete="off" checked> 1
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option6" id="option2" value="2" autocomplete="off"> 2
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option6" id="option3" value="3" autocomplete="off"> 3
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option6" id="option4" value="4" autocomplete="off"> 4
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="option6" id="option5" value="5" autocomplete="off"> 5
+      </label>
     <form action="ratings.php" method="post">
     <div class="form-group">
       <label for="ProsConsTextarea">Pros and Cons</label><br>
       <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
     </div>
+        
+     
+<!---star rating section of page BEGINS-->
+<br><br>
+<div class="">
+  <button type ="submit" name="submission" class="summit btn" role="button" >Summit</button>
+         
+     </div>
+
+<!---star rating section of page ENDS -->
+
+
     </form>
     <!---->
   </div>
-</div>
+@ -232,17 +80,51 @@ if( isset($_POST['submission']) ){
 
 <br>
 
@@ -115,6 +313,11 @@ if(isset($_GET['sid'])){
 
 <!--submit button-->
 <br><br><br>
+    
+         
+            <?php 
+    include 'footer.php';
+?>
 <input type="submit" value="Submit">
 <!----->
 
@@ -128,11 +331,3 @@ if(isset($_GET['sid'])){
 
 
 <!--bootstrap-->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<!--bootstrap-->
-
-</div>
-</body>
-</html>
